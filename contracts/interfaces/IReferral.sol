71e3086127212c809529618d14afe4f5b187d5c3
jc <pandangoup@outlook.com>
2025-11-11 04:39:44 +0800

    utils

--- contracts/interfaces/IReferral.sol
+++ contracts/interfaces/IReferral.sol
@@ -1,19 +1,19 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.13;
 
-interface IReferral{
-    
-    event BindReferral(address indexed user,address parent);
-    
-    function getReferral(address _address)external view returns(address);
+interface IReferral {
 
-    function isBindReferral(address _address) external view returns(bool);
+    event BindReferral(address indexed user, address parent);
 
-    function getReferralCount(address _address) external view returns(uint256);
+    function getReferral(address _address) external view returns (address);
 
-    function bindReferral(address _referral,address _user) external;
+    function isBindReferral(address _address) external view returns (bool);
 
-    function getReferrals(address _address,uint256 _num) external view returns(address[] memory);
+    function getReferralCount(address _address) external view returns (uint256);
 
-    function getRootAddress()external view returns(address);
+    function bindReferral(address _referral, address _user) external;
+
+    function getReferrals(address _address, uint256 _num) external view returns (address[] memory);
+
+    function getRootAddress() external view returns (address);
 }
