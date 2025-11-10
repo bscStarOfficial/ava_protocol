92d1d70fe9eac4d1e5e06f6020185548a940d853
jc <pandangoup@outlook.com>
2025-11-11 05:47:57 +0800

    Empty

--- contracts/Empty.sol
+++ contracts/Empty.sol
@@ -5,6 +5,7 @@ import "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
 import "@openzeppelin/contracts-upgradeable/proxy/utils/UUPSUpgradeable.sol";
 import "./interfaces/IManager.sol";
 
+// 空合约，提前部署设置ExcludedFromFee, 后续升级
 contract Empty is Initializable, UUPSUpgradeable {
     IManager public manager;
 
