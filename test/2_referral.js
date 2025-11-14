ce78efb713817f3f72388bf7b6d59f9a6429393c
jc <pandangoup@outlook.com>
2025-11-14 23:52:39 +0800

    ava test

--- test/2_referral.js
+++ test/2_referral.js
@@ -7,31 +7,20 @@ const common = require("./util/common");
 const {loadFixture} = require("@nomicfoundation/hardhat-network-helpers");
 
 let deployer, A, B, C, D, E, F, G;
-let ava;
+let registerV2;
 
 async function initialFixture() {
   await deployments.fixture();
 
-  [ava] = await common.getContractByNames(["AVA"]);
+  [registerV2] = await common.getContractByNames(["RegisterV2"]);
   [deployer, A, B, C, D, E, F, G] = await common.getAccounts(
     ["deployer", "A", "B", "C", "D", "E", "F", "G"]
   );
 }
 
-describe("发行", function () {
-  it('AVA总发行量131万枚', async function () {
-    expect(await ava.totalSupply()).to.equal(1310000 * 1e18);
-    expect(await ava.balanceOf(deployer)).to.equal(1310000 * 1e18);
-  });
+describe("注册", function () {
+  it('注册30代');
+  it('getReferral')
+  it('getReferrals')
 })
-describe("交易", function () {
-  before(async () => {
-    await initialFixture();
-  })
-  it('未开启预售无法交易')
-  it('黑名单地址无法转账')
-  it('买入手续费2.5%销毁、2.5构建流动性')
-  it('卖出手续费3%进入基金、2%进入技术')
-  it('30%盈利手续费进入profit')
-  it('无手续费地址交易不用手续费')
-})
+
