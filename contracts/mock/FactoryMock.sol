da30102f35e9e80995c40f5d6db4de9e82d21790
jc <pandangoup@outlook.com>
2025-11-14 18:11:39 +0800

    mock

--- contracts/mock/FactoryMock.sol
+++ contracts/mock/FactoryMock.sol
@@ -2,6 +2,6 @@
 import {UniswapV2Factory} from "@uniswap/v2-core/contracts/UniswapV2Factory.sol";
 
 
-contract RouterMock is UniswapV2Factory {
+contract FactoryMock is UniswapV2Factory {
     constructor(address _feeToSetter) UniswapV2Factory(_feeToSetter) public {}
 }
