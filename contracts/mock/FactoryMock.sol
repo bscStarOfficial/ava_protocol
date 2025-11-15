a5cbbe965a2c4b7a08418d9b5f9014f2cbbc814f
jc <pandangoup@outlook.com>
2025-11-16 02:27:17 +0800

    mock

--- contracts/mock/FactoryMock.sol
+++ contracts/mock/FactoryMock.sol
@@ -1,7 +1,2 @@
 // SPDX-License-Identifier: UNLICENSED
 import {UniswapV2Factory} from "@uniswap/v2-core/contracts/UniswapV2Factory.sol";
-
-
-contract FactoryMock is UniswapV2Factory {
-    constructor(address _feeToSetter) UniswapV2Factory(_feeToSetter) public {}
-}
--- contracts/mock/RouterMock.sol
+++ contracts/mock/RouterMock.sol
@@ -1,6 +1,2 @@
 // SPDX-License-Identifier: UNLICENSED
 import {UniswapV2Router02} from "@uniswap/v2-periphery/contracts/UniswapV2Router02.sol";
-
-contract RouterMock is UniswapV2Router02 {
-    constructor(address _factory, address _WETH) UniswapV2Router02(_factory, _WETH) public {}
-}
