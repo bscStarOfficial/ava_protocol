3806ca2f413d889fc473010d31e6a77b51bdd5b5
jc <pandangoup@outlook.com>
2025-11-16 17:15:22 +0800

    userBindReferral

---
+++ test/util/referral.js
@@ -0,0 +1,37 @@
+const {parseEther, formatEther, parseUnits} = require("ethers/lib/utils");
+const {ethers} = require("hardhat");
+const common = require("./common");
+const {setBalance} = require("@nomicfoundation/hardhat-network-helpers");
+const {BigNumber} = require("bignumber.js");
+const {Wallet} = require("ethers")
+
+let ava, usdt, referral, staking, root, provider;
+
+async function referralInit() {
+  [ava, usdt, referral, staking] = await common.getContractByNames(["AVA", 'USDT', 'Referral', 'AvaStaking']);
+  [root] = await common.getAccounts(["root"]);
+  provider = ethers.provider;
+}
+
+async function userBindReferral30() {
+  let wallets = [];
+
+  for (let i = 0; i < 30; i++) {
+    let wallet = Wallet.createRandom().connect(provider);
+    await setBalance(wallet.address, parseEther('100'));
+    let referrer = i == 0 ? root : wallets[i - 1].address;
+    await referral.connect(wallet).userBindReferral(referrer);
+
+    await usdt.transfer(wallet.address, parseEther('100000'));
+    await usdt.approve(staking.address, parseEther('100000'));
+
+    wallets.push(wallet);
+  }
+
+  return wallets;
+}
+
+module.exports = {
+  referralInit,
+  userBindReferral30
+}
